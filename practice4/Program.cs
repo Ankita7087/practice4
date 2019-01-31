ad4b35eaa75e024aad5723d870da86e59a69cce4
visualstudio <visualstudio@230-PC38>
2019-01-31 17:25:55 -0500

    coding

--- practice4/Program.cs
+++ practice4/Program.cs
@@ -10,6 +10,99 @@ namespace practice4
     {
         static void Main(string[] args)
         {
+            Countryside Ontario = new Countryside();
+            Ontario.run();
+
         }
     }
+
+    class LearningExample
+    {
+        public void run()
+        {
+            Village Toronto;
+            Village a;
+            Village b;
+            Toronto = new Village();
+            a = Toronto;
+
+            Toronto = new Village();
+            b = Toronto;
+
+            if (a == b) { Console.WriteLine("same"); }
+            else
+            { Console.WriteLine("different obj refs"); }
+        }
+    }
+    class Village
+    {
+        public static int numberOfVillages = 0;
+        public Village nextVillage;
+        public Village previousVillage;
+        public string VillageName;
+        public bool isAstrildeHere = false;
+
+        public Village()
+        {
+            Village.numberOfVillages++;
+        }
+    }
+
+    class Countryside
+    {
+        public Village Maple;
+        public Village Toronto;
+        public Village Ajax;
+        public Village Head;
+        public Village Tail;
+        public Village Current;
+
+        public void run()
+        {
+            this.MapInitializer();
+            this.LookForAstrilde();
+            Console.WriteLine("Hugi found Astrilde in " + Current.VillageName);
+        }
+
+        public void MapInitializer()
+        {
+            Ajax = new Village();
+            Toronto = new Village();
+            Maple = new Village();
+
+            Maple.VillageName = "Maple";
+            Maple.previousVillage = null;
+            Maple.nextVillage = Toronto;
+            Ajax.isAstrildeHere = true;
+
+            Toronto.previousVillage = Maple;
+            Toronto.VillageName = "Toronto";
+            Toronto.nextVillage = Ajax;
+
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.previousVillage = Toronto;
+
+        }
+        public void LookForAstrilde()
+        {
+            Current = Maple;
+            while (Current.nextVillage != null)
+            {
+                if (Current.isAstrildeHere)
+                {
+                    Console.WriteLine("found astrilde");
+                    return;
+                }
+                else
+                {
+                    Current = Current.nextVillage;
+                }
+            }
+        }
+
+
+
+    }
+
 }
